972846d80b8eac3fe9060aab58a8f504e23d9fef
Karem Alcalá <kalcala@comscore.com>
2021-02-27 23:07:24 -0300

    estructura basica

--- index.py
+++ index.py
@@ -2,23 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Creating simple Routes 
-@app.route('/test')
-def test():
-    return "Home Page"
-
-@app.route('/test/about/')
-def about_test():
-    return "About Page"
-
 # Routes to Render Something
 @app.route('/')
 def home():
     return render_template("home.html")
 
-@app.route('/about', strict_slashes=False)
-def about():
-    return render_template("about.html")
+@app.route('/products', strict_slashes=False)
+def products():
+    return render_template("products.html")
+
+""" @app.route('/*')
+def products():
+    return render_template("error.html") """
 
 # Make sure this we are executing this file
 if __name__ == '__main__':
